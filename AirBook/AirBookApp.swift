39d3d3f466a3bffc4b3e001f85f82fa4479eaa52
Husni Zayyin Ansori <45607060+husnizayyin@users.noreply.github.com>
2023-12-15 15:04:32 +0700

    Initial Commit

---
+++ AirBook/AirBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBookApp.swift
+//  AirBook
+//
+//  Created by Husni Zayyin Ansori on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AirBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
